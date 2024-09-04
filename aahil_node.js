e3a610f56d0644670ced655e7a5a304903229f31
= <=>
2024-09-04 07:33:01 +0530

    "Updated port number from 5050 to 5112"

--- aahil_node.js
+++ aahil_node.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const port = process.env.PORT || 5050
+const port = process.env.PORT || 5112
 app.get('/' , (req , res)=>{
    res.send('hello from simple server :)');
 })
