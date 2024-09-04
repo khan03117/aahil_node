0885ce5346429467e8d0181519f78615f0267653
= <=>
2024-09-04 07:58:06 +0530

    changed

--- aahil_node.js
+++ aahil_node.js
@@ -4,20 +4,19 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
-
-// Load SSL certificate and key
-const options = {
-   key: fs.readFileSync(path.resolve(__dirname, './ssl/private.pem')),
-   cert: fs.readFileSync(path.resolve(__dirname, './ssl/cert.pem')),
-};
-
-const port = 8111;
-
+// // Load SSL certificate and key
+// const options = {
+//    key: fs.readFileSync(path.resolve(__dirname, './ssl/private.pem')),
+//    cert: fs.readFileSync(path.resolve(__dirname, './ssl/cert.pem')),
+// };
+const port = 5050;
 app.get('/', (req, res) => {
    res.send('Hello from simple server :)');
 });
-
+app.listen(port, () => {
+   console.log(`Server is running on port ${port}`);
+ });
 // Create HTTPS server
-https.createServer(options, app).listen(port, () => {
-   console.log('Server is running on https://localhost:' + port);
-});
+// https.createServer(options, app).listen(port, () => {
+//    console.log('Server is running on https://localhost:' + port);
+// });
