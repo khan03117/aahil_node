9b705fb51161824b7ef05c74559cb5fce3b3751d
= <=>
2024-09-08 07:19:52 +0530

    "Removed HTTPS and file system dependencies, updated server listen configuration"

--- aahil_node.js
+++ aahil_node.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
+// const https = require('https');
+// const fs = require('fs');
+// const path = require('path');
 require('dotenv').config();
 // // Load SSL certificate and key
 // const options = {
@@ -13,9 +13,9 @@ const port = 5050;
 app.get('/', (req, res) => {
    res.send('Hello from simple server :)');
 });
-app.listen(port, () => {
-   console.log(`Server is running on port ${port}`);
- });
+app.listen(5050, '0.0.0.0', () => {
+   console.log('Server is running on port 5050');
+});
 // Create HTTPS server
 // https.createServer(options, app).listen(port, () => {
 //    console.log('Server is running on https://localhost:' + port);
